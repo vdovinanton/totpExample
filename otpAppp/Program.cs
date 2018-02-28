bd6f6113c6d34505f249d5211c9e323c3f62fb82
anton vdovin <antonvdovin92@gmail.com>
2018-02-28 16:52:30 +0200

    initial

---
+++ otpAppp/Program.cs
@@ -0,0 +1,40 @@
+﻿using OtpSharp;
+using Serilog;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Base32;
+
+
+namespace otpAppp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .CreateLogger();
+
+            Console.Write("Input key: ");
+            string key = Console.ReadLine();
+
+            var task = Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    byte[] decodedKey = Base32Encoder.Decode(key);
+                    var otp = new Totp(decodedKey);
+
+                    Log.Information(otp.ComputeTotp());
+
+                    Thread.Sleep(1000);
+                }
+
+            }, TaskCreationOptions.LongRunning);
+
+            Task.WaitAll(task);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ otpAppp/TotpService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OtpSharp;
+using Base32;
+
+namespace otpAppp
+{
+    public interface IMfaService
+    {
+        Task ValidateAsync(string token);
+    }
+
+    public class TotpService
+    {
+        private readonly static string _secretKey = "qweqweqweqw";
+        public bool ValidateAsync(string token)
+        {
+            long timeStepMatched = 0;
+
+            byte[] decodedKey = Base32Encoder.Decode(_secretKey);
+            var otp = new Totp(decodedKey);
+            otp.ComputeTotp();
+            bool valid = otp.VerifyTotp(token, out timeStepMatched, new VerificationWindow(2, 2));
+
+            return valid;
+        }
+    }
+}
